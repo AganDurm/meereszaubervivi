a364f304805fd7d6c543531e182ef0dc6712e9c3
AganDurm <Agan.Durmisevic@quinscape.de>
2022-06-02 22:47:38 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './web/header/header.component';
+import {FooterComponent} from './web/footer/footer.component';
+import {SurfingComponent} from './cool/surfing/surfing.component';
+import {PlaneComponent} from './cool/plane/plane.component';
+import {CloudsComponent} from './cool/clouds/clouds.component';
+import {PreFooterComponent} from './web/pre-footer/pre-footer.component';
+import {ContentComponent} from './web/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SurfingComponent,
+    PlaneComponent,
+    CloudsComponent,
+    PreFooterComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
